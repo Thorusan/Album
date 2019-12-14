6c6ad2e0f0d1735ea0a387b0b74d93dd48c4ac14
Ales <ales.kavcic86@gmail.com>
2019-12-14 15:27:07 +0100

    Added companion object for Retrofit

--- app/src/main/java/com/example/album/network/ApiService.kt
+++ app/src/main/java/com/example/album/network/ApiService.kt
@@ -6,17 +6,32 @@ import com.example.album.datamodel.AlbumData
 import com.example.album.datamodel.PhotoData
 import io.reactivex.Observable
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
     @GET(BASE_URL + "/users")
-    fun getUsers(): Observable<Response<List<UserData.User>>>
+    fun getUsers(): Observable<List<UserData.User>>
 
     @GET(BASE_URL + "/albums")
-    fun getUserAlbums(@Query("userId") userId: Int): Observable<Response<List<AlbumData>>>
+    fun getUserAlbums(@Query("userId") userId: Int): Observable<List<AlbumData>>
 
-    @GET(BASE_URL + "/photos ")
-    fun getAlbumPhotos(@Query("albumId") albumId: Int): Observable<Response<List<PhotoData>>>
+    @GET(BASE_URL + "/photos/")
+    fun getAlbumPhotos(@Query("albumId") albumId: Int): Observable<List<PhotoData>>
+
+
+    companion object {
+        fun create(): ApiService {
+            val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+    }
 
 }
