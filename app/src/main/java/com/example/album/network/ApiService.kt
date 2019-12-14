f6e106b65c9067b13770ed39aa8fafea109fcc19
Ales <ales.kavcic86@gmail.com>
2019-12-14 15:07:57 +0100

    Added REST method calls for getting: Users, Album by user's id and Photos by album's id.

--- app/src/main/java/com/example/album/network/ApiService.kt
+++ app/src/main/java/com/example/album/network/ApiService.kt
@@ -2,13 +2,21 @@ package com.example.album.network
 
 import com.example.album.datamodel.UserData
 import com.example.album.common.Constants.Companion.BASE_URL
+import com.example.album.datamodel.AlbumData
+import com.example.album.datamodel.PhotoData
 import io.reactivex.Observable
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
     @GET(BASE_URL + "/users")
     fun getUsers(): Observable<Response<List<UserData.User>>>
 
+    @GET(BASE_URL + "/albums")
+    fun getUserAlbums(@Query("userId") userId: Int): Observable<Response<List<AlbumData>>>
+
+    @GET(BASE_URL + "/photos ")
+    fun getAlbumPhotos(@Query("albumId") albumId: Int): Observable<Response<List<PhotoData>>>
 
 }
